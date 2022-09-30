d6fe9dd380d1e3d2d65d8752decbf47673444b22
Iván Fernández <ifernandez@middle-sea.com>
2022-09-30 18:55:32 -0300

    se ordenan vértices

--- scripts/sketch.js
+++ scripts/sketch.js
@@ -44,23 +44,23 @@ function dibujarCieloAzul() {
 
   beginShape();
   vertex(ladoDelCuadrado, ladoDelCuadrado);
-  vertex(ladoDelCuadrado, ladoDelCuadrado * 11.33);
-  vertex(ladoDelCuadrado * 1.5 - padding, ladoDelCuadrado * 10.5);
-  vertex(ladoDelCuadrado * 1.5, ladoDelCuadrado * 10.5);
-  vertex(ladoDelCuadrado * 2.66, ladoDelCuadrado * 8.75);
-  vertex(ladoDelCuadrado * 3, ladoDelCuadrado * 9);
-  vertex(ladoDelCuadrado * 4 - padding, ladoDelCuadrado * 8);
-  vertex(ladoDelCuadrado * 4.5, ladoDelCuadrado * 8.66);
-  vertex(ladoDelCuadrado * 8.25, ladoDelCuadrado * 3.33);
-  vertex(ladoDelCuadrado * 8.33, ladoDelCuadrado * 3.50);
-  vertex(ladoDelCuadrado * 8.50, ladoDelCuadrado * 3.33);
-  vertex(ladoDelCuadrado * 9 - padding, ladoDelCuadrado * 3.66);
-  vertex(ladoDelCuadrado * 10.33, ladoDelCuadrado * 1.75);
-  vertex(ladoDelCuadrado * 10.75, ladoDelCuadrado * 2);
-  vertex(ladoDelCuadrado * 11.1, ladoDelCuadrado * 1.9);
-  vertex(ladoDelCuadrado * 11.6, ladoDelCuadrado * 2.75);
-  vertex(ladoDelCuadrado * 12, ladoDelCuadrado * 2.8);
   vertex(ladoDelCuadrado * 11, ladoDelCuadrado);
+  vertex(ladoDelCuadrado * 12, ladoDelCuadrado * 2.8);
+  vertex(ladoDelCuadrado * 11.6, ladoDelCuadrado * 2.75);
+  vertex(ladoDelCuadrado * 11.1, ladoDelCuadrado * 1.9);
+  vertex(ladoDelCuadrado * 10.75, ladoDelCuadrado * 2);
+  vertex(ladoDelCuadrado * 10.33, ladoDelCuadrado * 1.75);
+  vertex(ladoDelCuadrado * 9 - padding, ladoDelCuadrado * 3.66);
+  vertex(ladoDelCuadrado * 8.50, ladoDelCuadrado * 3.33);
+  vertex(ladoDelCuadrado * 8.33, ladoDelCuadrado * 3.50);
+  vertex(ladoDelCuadrado * 8.25, ladoDelCuadrado * 3.33);
+  vertex(ladoDelCuadrado * 4.5, ladoDelCuadrado * 8.66);
+  vertex(ladoDelCuadrado * 4 - padding, ladoDelCuadrado * 8);
+  vertex(ladoDelCuadrado * 3, ladoDelCuadrado * 9);
+  vertex(ladoDelCuadrado * 2.66, ladoDelCuadrado * 8.75);
+  vertex(ladoDelCuadrado * 1.5, ladoDelCuadrado * 10.5);
+  vertex(ladoDelCuadrado * 1.5 - padding, ladoDelCuadrado * 10.5);
+  vertex(ladoDelCuadrado, ladoDelCuadrado * 11.33);
   vertex(ladoDelCuadrado, ladoDelCuadrado);
   endShape();
   pop();
@@ -73,13 +73,13 @@ function dibujarCieloRojo() {
 
   beginShape();
   vertex(ladoDelCuadrado * 11, ladoDelCuadrado);
-  vertex(ladoDelCuadrado * 12, ladoDelCuadrado * 2.8);
-  vertex(ladoDelCuadrado * 16.5, ladoDelCuadrado * 9 - padding);
-  vertex(ladoDelCuadrado * 17.25, ladoDelCuadrado * 8 - padding);
-  vertex(ladoDelCuadrado * 17.66, ladoDelCuadrado * 8.66);
-  vertex(ladoDelCuadrado * 17.95, ladoDelCuadrado * 8.5);
-  vertex(ladoDelCuadrado * 19, ladoDelCuadrado * 11.33);
   vertex(ladoDelCuadrado * 19, ladoDelCuadrado);
+  vertex(ladoDelCuadrado * 19, ladoDelCuadrado * 11.33);
+  vertex(ladoDelCuadrado * 17.95, ladoDelCuadrado * 8.5);
+  vertex(ladoDelCuadrado * 17.66, ladoDelCuadrado * 8.66);
+  vertex(ladoDelCuadrado * 17.25, ladoDelCuadrado * 8 - padding);
+  vertex(ladoDelCuadrado * 16.5, ladoDelCuadrado * 9 - padding);
+  vertex(ladoDelCuadrado * 12, ladoDelCuadrado * 2.8);
   vertex(ladoDelCuadrado * 11, ladoDelCuadrado);
   endShape();
   pop();
@@ -102,6 +102,7 @@ function dibujarCumbreDeMontaña() {
   vertex(ladoDelCuadrado * 11.6, ladoDelCuadrado * 2.75);
   vertex(ladoDelCuadrado * 12, ladoDelCuadrado * 2.8);
   vertex(ladoDelCuadrado * 12.9, ladoDelCuadrado * 4);
+  vertex(ladoDelCuadrado * 7.8, ladoDelCuadrado * 4);
   endShape();
   pop();
 }
