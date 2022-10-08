e12284381173d4dbdd30dfcafd4f176d33319901
Iván Fernández <ifernandez@middle-sea.com>
2022-10-08 04:56:08 -0300

    se implementa dibujarCuadro

--- sketches/voronoi/sketch.js
+++ sketches/voronoi/sketch.js
@@ -1,10 +1,27 @@
 const tamañoDeCanvas = 750;
+const tamañoDeCuadro = 600;
 
 function setup() {
     const canvas = createCanvas(tamañoDeCanvas, tamañoDeCanvas);
     canvas.parent('voronoi');
+    background('red');
+    strokeWeight(5);
+    stroke('white');
 }
 
 function draw() {
-    background(51);
+    dibujarCuadro(tamañoDeCanvas, tamañoDeCuadro);
+}
+
+function mousePressed() {
+    point(mouseX, mouseY);
+}
+
+function dibujarCuadro(tamañoDeCanvas, tamañoDeCuadro) {
+    const distanciaParaCentrar = tamañoDeCanvas - tamañoDeCuadro;
+
+    push()
+    fill(51);
+    rect(distanciaParaCentrar / 2, distanciaParaCentrar / 8, tamañoDeCanvas - distanciaParaCentrar, tamañoDeCanvas - distanciaParaCentrar);
+    pop();
 }
