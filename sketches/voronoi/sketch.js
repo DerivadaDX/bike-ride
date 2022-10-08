a3b9ad2059267d9f0f4c9c0adb78c8e12440ce56
Iván Fernández <ifernandez@middle-sea.com>
2022-10-08 04:33:24 -0300

    se agregó sketch para voronoi

---
+++ sketches/voronoi/sketch.js
@@ -0,0 +1,10 @@
+const tamañoDeCanvas = 900;
+
+function setup() {
+    const canvas = createCanvas(tamañoDeCanvas, tamañoDeCanvas);
+    canvas.parent('voronoi');
+}
+
+function draw() {
+    background(51);
+}
